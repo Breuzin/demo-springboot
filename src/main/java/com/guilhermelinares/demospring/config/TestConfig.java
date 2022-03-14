9987f4c9cadebd3e1f281b23d5341672373cf870
Breuzin <guimine.20@gmail.com>
2022-03-14 20:44:55 -0300

    Payment, one-to-one association

--- src/main/java/com/guilhermelinares/demospring/config/TestConfig.java
+++ src/main/java/com/guilhermelinares/demospring/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.guilhermelinares.demospring.entities.Category;
 import com.guilhermelinares.demospring.entities.Order;
 import com.guilhermelinares.demospring.entities.OrderItem;
+import com.guilhermelinares.demospring.entities.Payment;
 import com.guilhermelinares.demospring.entities.Product;
 import com.guilhermelinares.demospring.entities.User;
 import com.guilhermelinares.demospring.entities.enums.OrderStatus;
@@ -79,6 +80,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }

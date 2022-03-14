51d37896d96a429306366554a989e9bd265d4670
Breuzin <guimine.20@gmail.com>
2022-03-14 17:12:59 -0300

    OrderStatus Enum

--- src/main/java/com/guilhermelinares/demospring/entities/Order.java
+++ src/main/java/com/guilhermelinares/demospring/entities/Order.java
@@ -13,6 +13,7 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.guilhermelinares.demospring.entities.enums.OrderStatus;
 
 @Entity
 @Table(name = "tb_order")
@@ -26,6 +27,7 @@ public class Order implements Serializable{
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
 	
+	private Integer orderStatus; 
 	
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -34,10 +36,11 @@ public class Order implements Serializable{
 	public Order() {
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);;
 		this.client = client;
 	}
 
@@ -55,6 +58,15 @@ public class Order implements Serializable{
 		this.moment = moment;
 	}
 
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) { 
+			this.orderStatus = orderStatus.getCode();
+		}
+	}
+
 	public User getClient() {
 		return client;
 	}
@@ -62,6 +74,7 @@ public class Order implements Serializable{
 		this.client = client;
 	}
 
+	
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);

6b8566bcd8d9e359b1f7c2245612e456d08707d4
Guilherme Linares <guimine.20@gmail.com>
2022-03-15 12:38:00 -0300

    Exception handling - update

--- src/main/java/com/guilhermelinares/demospring/services/UserService.java
+++ src/main/java/com/guilhermelinares/demospring/services/UserService.java
@@ -3,7 +3,8 @@ package com.guilhermelinares.demospring.services;
 import java.util.List;
 import java.util.Optional;
 
-import org.hibernate.ResourceClosedException;
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,14 +45,19 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getById(id);
 		updateData(entity, obj);
 		return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
+		
 	}
 }
